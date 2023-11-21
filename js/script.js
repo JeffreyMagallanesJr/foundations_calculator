b8d6915ad6fd6826bf412fec629e0cb072becd6d
Jeffrey Magallanes Jr <79354016+JeffreyMagallanesJr@users.noreply.github.com>
2023-11-21 23:23:49 +0800

    Add operate function

--- js/script.js
+++ js/script.js
@@ -6,10 +6,32 @@ function subtract(a, b) {
     return a - b;
 }
 
-function multiply (a, b) {
+function multiply(a, b) {
     return a * b;
 }
 
-function divide (a, b) {
+function divide(a, b) {
     return a / b;
 }
+
+function operate(firstNumber,operator,secondNumber) {
+    // switch(operator) {
+    //     case "+":
+    //         return add(firstNumber,secondNumber);
+    //         break;
+    //     case "-":
+    //         return subtract(firstNumber,secondNumber);
+    //         break;
+    //     case "*":
+    //         return multiply(firstNumber,secondNumber);
+    //         break;
+    //     case "/":
+    //         return divide(firstNumber,secondNumber);
+    //         break;
+    // }
+    return operator(firstNumber,secondNumber);
+}
+
+let firstNumber;
+let operator;
+let secondNumber;
