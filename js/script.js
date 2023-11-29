9ca68f8c5f342da7ce018a05e5785cd630db23e8
Jeffrey Magallanes Jr <79354016+JeffreyMagallanesJr@users.noreply.github.com>
2023-11-29 22:45:47 +0800

    Change displayNumber to populateDisplay

--- js/script.js
+++ js/script.js
@@ -18,33 +18,35 @@ function operate(firstNumber,operator,secondNumber) {
     return operator(firstNumber,secondNumber);
 }
 
-function displayNumber(number) {
+function populateDisplay(number) {
     display.textContent += number;
+    displayValue = Number(display.textContent);
 }
 
-const operatorSymbols = ["+","-","*","/"];
-const operatorNames = ["add","subtract","multiply","divide"];
 let firstNumber;
 let operator;
 let secondNumber;
+let displayValue;
 
 let display = document.querySelector("#display");
 
 let numbers = Array.from(document.querySelectorAll(".number"));
 
-numbers.map(function (number) {
+numbers.forEach(function (number) {
     number.addEventListener("click", function() {
-        displayNumber(number.textContent);
+        populateDisplay(number.textContent);
     })
 });
 
+const operatorSymbols = ["+","-","*","/"];
+const operatorNames = ["add","subtract","multiply","divide"];
+
 let operators = Array.from(document.querySelectorAll(".operator"));
-
-
 
 let operatorsSymbols = operators.map(function (item) {
     return item.textContent;
 })
+
 let operatorsNames;
 
 operatorsNames = operatorsSymbols.map(function (item) {
